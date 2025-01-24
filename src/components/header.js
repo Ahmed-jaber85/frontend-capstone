7973988773b2d3ab9e02b2f4089f3b112dbbd1ce
ahmed alfatih <ahmedjaber1852@gmail.com>
2025-01-24 15:04:59 +0300

    final commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import '../css/header.css';
-function Header() {
+import React, { useState} from 'react';
+import "../components/styles/Header.css"
+import Logo from "../components/assets/logo.jpg"
+import { NavLink } from 'react-router-dom';
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+
+const Header = () => {
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
-    <header className="header">
-      <div className="logo">Logo</div>
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-item"><a href="#about">About</a></li>
-          <li className="nav-item"><a href="#menu">Menu</a></li>
-          <li className="nav-item"><a href="#book">Book</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    <>
+      <header className="header-container">
+        <NavLink to = "/">
+            {<img className="logo-img" src = { Logo } alt = "Little Lemon logo" />}
+        </NavLink>
+        <nav>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="#">About</NavLink></li>
+                <li><NavLink to="#">Menu</NavLink></li>
+                <li><NavLink to="/reservations">Reservations</NavLink></li>
+                <li><NavLink to="#">Order Online</NavLink></li>
+                <li><NavLink to="#">Login</NavLink></li>
+            </ul>
+        </nav>
+        <div className="hamburger" onClick = {handleClick}>
+            {click ? (<AiOutlineClose size={20} style={{ color: "#333333"}}/>) : (<AiOutlineMenu size={30} style={{ color: "#333333", borderLeft: "1px solid #333333", paddingLeft: "10px", height: "20px"}}/>)}  
+        </div>
+      </header>
+    </>
+  )
 }
 
-export default Header;
-
-
+export default Header
